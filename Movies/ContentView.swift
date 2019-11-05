ff3dbd742e426fa1ee2273df75e9c2001adab872
Frank S. Bara <fbara1@yahoo.com>
2019-11-05 13:22:16 -0600

    Start building basic movie UI.

--- Movies/ContentView.swift
+++ Movies/ContentView.swift
@@ -8,9 +8,71 @@
 
 import SwiftUI
 
+struct Movie: Identifiable, Hashable {
+    let id = UUID.init()
+    let name, imageName: String
+}
+
 struct ContentView: View {
+    
+    let movies: [[Movie]] = [
+        [.init(name: "Deadpool", imageName: "deadpool"),
+        .init(name: "The Shape of Water", imageName: "water")],
+        [.init(name: "Incredibles 2", imageName: "incredibles"),
+        .init(name: "Tomb Raider", imageName: "raider")],
+        [.init(name: "Deadpool", imageName: "deadpool"),
+        .init(name: "The Shape of Water", imageName: "water")],
+        [.init(name: "Incredibles 2", imageName: "incredibles"),
+        .init(name: "Tomb Raider", imageName: "raider")]
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geo in
+            
+            ScrollView {
+                
+                VStack(spacing: 16) {
+                    Text("BaraLabs Movies")
+                        .font(.system(size: 24, weight: .semibold))
+                    ForEach(self.movies, id: \.self) { row in
+                        HStack(spacing: 24) {
+                            ForEach(row) { movie in
+                                VStack(spacing: 12) {
+                                    Image(movie.imageName)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: (geo.size.width - 48) / 2, height: 250)
+                                        .clipped()
+                                        .cornerRadius(8)
+                                    
+                                    Text(movie.name)
+                                        .font(.system(size: 15, weight: .bold))
+                                    
+                                    Button(action: {
+                                        
+                                    }) {
+                                        Text("Buy Tickets")
+                                            .foregroundColor(.white)
+                                            .padding(.horizontal, 16)
+                                            .padding(.vertical, 8)
+                                            .background(Color.red)
+                                            .cornerRadius(8)
+                                            .shadow(radius: 10)
+                                    }
+                                }
+                            }
+                        }
+                        
+                    }
+                    
+                    Spacer()
+                    
+                }
+            }
+        }
+        
+        
+        
     }
 }
 
